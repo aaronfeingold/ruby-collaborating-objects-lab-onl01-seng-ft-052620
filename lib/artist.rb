6ce6c7d6bb863fd73b91c33e6afa78609c2f33a8
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 15:30:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,17 @@ class Artist
     song.artist = self
   end 
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
   def save
     @@all << self
   end  
