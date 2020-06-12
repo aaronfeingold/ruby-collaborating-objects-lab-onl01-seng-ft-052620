a3f25c2357743ec3464c8f9b31b1c19945a5976c
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 15:30:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,20 @@
-class Artist 
-  attr_accessor :name, :songs 
-  @@all = [] 
-  
-  def initialize (name)
+class Artist
+  @@all = []
+  attr_accessor :name, :songs
+
+  def initialize(name)
     @name = name
-    save 
-  end  
-  
+    save
+  end
+
+  def self.all
+    @@all    
+  end
+
   def add_song(song)
     song.artist = self
-  end 
-  
+  end
+
   def songs
     Song.all.select {|song| song.artist == self}
   end
@@ -22,12 +26,12 @@ class Artist
   def self.find(name)
     self.all.find {|artist| artist.name == name }
   end
+
   def save
     @@all << self
-  end  
-  
-  def self.all
-    @@all
   end
-  
-end 
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
+end
